af27c052192844da4adbdff43df90bd240fb96e3
Irina Gorbachova <Iracka@irinas-macbook-pro.local>
2020-04-11 18:12:32 +0300

    Initial Commit

---
+++ LoyalS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoyalS
+//
+//  Created by Irachka on 4/11/20.
+//  Copyright © 2020 Iryna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
