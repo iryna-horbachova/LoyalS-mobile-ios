61afd6244948a8c72ae401a8c131f32ee1b1ed3c
iryna-horbachova <irachka.gorbacheva@gmail.com>
2020-06-20 17:35:34 +0300

    Fixed signin/signup issues

---
+++ LoyalS/Discounts/UsedDiscountTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UsedDiscountTableViewCell.swift
+//  LoyalS
+//
+//  Created by Irachka on 6/20/20.
+//  Copyright © 2020 Iryna. All rights reserved.
+//
+
+import UIKit
+
+class UsedDiscountTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
