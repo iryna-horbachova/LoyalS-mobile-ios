07edb5f3e4043385ad42a1608adbc4e0d56a5493
iryna-horbachova <irachka.gorbacheva@gmail.com>
2020-06-20 18:27:21 +0300

    Added check in functionality

---
+++ LoyalS/Places/CheckInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CheckInViewController.swift
+//  LoyalS
+//
+//  Created by Irachka on 6/20/20.
+//  Copyright © 2020 Iryna. All rights reserved.
+//
+
+import UIKit
+
+class CheckInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
